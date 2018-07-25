e206a1315392eb2f94fa3eaf6b7e5ac8884e680d
sulinehk <sulinehk@gmail.com>
2018-07-26 02:01:40 +0800

    Fix BinarySearch.go style by gofmt (#80)
    * Fix BinarySearch.go style by gofmt
    
    * Fix longest_common_subsequence.go style by gofmt

--- 09_dynamic_programming/golang/01_longest_common_subsequence.go
+++ 09_dynamic_programming/golang/01_longest_common_subsequence.go
@@ -1,10 +1,11 @@
 package main
+
 import "fmt"
 
-func main(){
+func main() {
 	if word_a[i] == word_b[j] {
-		cell[i][j] = cell[i-1][j-1]+1
-	}else{
+		cell[i][j] = cell[i-1][j-1] + 1
+	} else {
 		cell[i][j] = max(cell[i-1][j], cell[i][j-1])
 	}
 }
