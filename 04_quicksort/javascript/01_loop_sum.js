3e887a6d3c9612d4d452ac940099204b308da683
Johnny Bell <hi@johnnybell.io>
2018-10-18 08:26:17 -0700

    updated the 04_quicksort/javascript/01_loop_sum.js to a working soloution (#88)

--- 04_quicksort/javascript/01_loop_sum.js
+++ 04_quicksort/javascript/01_loop_sum.js
@@ -1,18 +1,15 @@
 /**
  * Sums values in array by loop "for"
  * @param {Array} arr Array of numbers
- * @return {number} Sum of the numbers
+ * @return {total} Sum of the numbers
  */
-function sumLoop( arr ) {
-    var result = 0;
 
-    for ( var i = 0; i < newArr.length; i++ ) {
-        result += newArr[i];
+function sum(arr) {
+    let total = 0;
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
     }
-
-    return result;
+    return total;
 }
 
-var arr = [1, 2, 3, 4];
-
-console.log( sumLoop( arr ) ); // 10
+console.log(sum([1, 2, 3, 4])); // 10
