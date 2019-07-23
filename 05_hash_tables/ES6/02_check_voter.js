84b6d19416d27549d92a1f0a09002bc2444b9935
Alexandrshy <alexandrshy@gmail.com>
2019-07-23 08:37:29 +0400

    Fixed formatting problems

--- 05_hash_tables/ES6/02_check_voter.js
+++ 05_hash_tables/ES6/02_check_voter.js
@@ -1,14 +1,18 @@
 const voted = {};
-const checkVoter = (name) => {
+
+/**
+ * Vote check
+ * @param {string} name Voter name
+ */
+const checkVoter = name => {
   if (voted[name]) {
-    console.log('kick them out!');
+    console.log("kick them out!");
   } else {
     voted[name] = true;
-    console.log('let them vote!');
+    console.log("let them vote!");
   }
 };
 
-
-checkVoter('tom'); // let them vote!
-checkVoter('mike'); // let them vote!
-checkVoter('mike'); // kick them out!
+checkVoter("tom"); // let them vote!
+checkVoter("mike"); // let them vote!
+checkVoter("mike"); // kick them out!
--- 05_hash_tables/javascript/02_check_voter.js
+++ 05_hash_tables/javascript/02_check_voter.js
@@ -1,15 +1,19 @@
-'use strict';
+"use strict";
 
 const voted = {};
+
+/**
+ * Vote check
+ * @param {string} name Voter name
+ */
 function check_voter(name) {
   if (voted[name]) {
-    console.log('kick them out!');
+    console.log("kick them out!");
   } else {
     voted[name] = true;
-    console.log('let them vote!');
+    console.log("let them vote!");
   }
 }
-
 
 check_voter("tom"); // let them vote!
 check_voter("mike"); // let them vote!
