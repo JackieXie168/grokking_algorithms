a76c5e5d7d51ffc938f155f17744f6f916e74feb
Alexandrshy <alexandrshy@gmail.com>
2019-07-27 02:00:06 +0400

    Fixed formatting problems, added JSDoc, deleted duplicate example

--- 09_dynamic_programming/ES6/examples/base.js
+++
@@ -1,7 +0,0 @@
-export const initializeMatrix = (rows, cols) => {
-  const matrix = [];
-  for (let i = 0; i < rows.length; i += 1) {
-    matrix.push(Array(cols.length).fill(0));
-  }
-  return matrix;
-};
