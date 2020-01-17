891eec2873683e4c876b20c3cb3a7b3355caefb8
Ruslan Korolev <great1672@gmail.com>
2020-01-17 18:26:21 +0200

    fix 04_recursive_max on ruby

--- 04_quicksort/ruby/04_recursive_max.rb
+++ 04_quicksort/ruby/04_recursive_max.rb
@@ -1,10 +1,12 @@
 def max(list)
-  if list.empty?
-    nil
-  elsif list.length == 1
-    numbers[0]
+  return nil if list.empty?
+
+  if list.length == 1
+    list[0] # base case
   else
     sub_max = max(list[1..-1])
     list[0] > sub_max ? list[0] : sub_max
   end
 end
+
+puts max([2,3,8,5,5])
