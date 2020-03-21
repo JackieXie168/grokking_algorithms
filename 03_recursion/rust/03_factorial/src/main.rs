24c91a4a0b987a6889f6803e5390b79c4762418b
Alexander Launi <alex.launi@gmail.com>
2020-03-21 17:20:03 -0400

    Implement factorial using generic mathematics

--- 03_recursion/rust/03_factorial/src/main.rs
+++ 03_recursion/rust/03_factorial/src/main.rs
@@ -1,9 +1,17 @@
-fn fact(x: u32) -> u32 {
-    if x == 1 {
-        return 1;
+use std::ops::Sub;
+use std::fmt::Display;
+use num_traits::identities::One;
+
+fn fact<T: PartialOrd + PartialEq + One + Sub<Output = T> + Copy + Display>(x: T) -> T {
+    if x < T::one() {
+        panic!("Invalid number: {}", x);
     }
 
-    return x * fact(x - 1);
+    if x.is_one() {
+        return T::one();
+    }
+
+    return x * fact(x - T::one());
 }
 
 fn main() {
