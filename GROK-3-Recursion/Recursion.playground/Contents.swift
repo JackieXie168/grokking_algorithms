3ae2f5014fd82b89fe4b4e632d1531f2837ef296
Dima <dmi.aksyonov@gmail.com>
2021-08-01 15:07:06 +0300

    GROK-3: updated contents

--- GROK-3-Recursion/Recursion.playground/Contents.swift
+++ GROK-3-Recursion/Recursion.playground/Contents.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-// MARK: - Recursion: How Many in The Boxes?
+// MARK: - Recursion: What's in The Box?
 
 /**
 What we do here is create an array of nested arrays.
@@ -9,14 +9,28 @@ Nestendess level: 3 nested boxes.
 Goal: find total count.
 */
 
-/// Create first box with ints
-let box0 = [1, 2, 3]
+/// Convenience methods creates a box with random elements count (between 0 and 10 count)
+/// Elements value can be from 0 to 10 for simplicity
+/// - Returns: Box with ints
+func makeBox() -> [Int] {
+	(0...Int.random(in: 0...10)).map({ $0 })
+}
 
-/// Increasing nestednes (up to 3)
-let box1 = [4, box0, 5] as [Any]
-let chest = [box1, 44, 5, 66, 7, box1, 6, 6, 4, 4, box0] as [Any]
-let chests = [chest, chest]
-
+/// Makes a pseudo-random box of boxes
+/// - Returns: the box of boxes
+func makeNestedBox() -> [Any] {
+	let random = Int.random(in: 0...10)
+	
+	let array = (0...random).map({ element in
+		if element % 2 == 0 {
+			return element
+		} else {
+			return makeBox()
+		}
+	}) as [Any]
+	
+	return array
+}
 
 /// Calculates and returns total count of boxes recursively
 /// - Parameter boxes: Boxes array, any nestedness
@@ -49,29 +63,64 @@ func countBoxesRecursively(_ boxes: [Any]) -> Int {
 	return count
 }
 
-// MARK: - Example with Factorial
 
+/// Create first box with ints
+let box0 = makeBox()
+
+/// Increasing nestednes to 1 level
+let box1 = [4, box0, 5] as [Any]
+
+/// Increasing nestedness to 2 levels
+let chest = [box1, 44, 5, 66, 7, makeBox(), 6, 6, 4, 4, box0] as [Any]
+
+/// Increasing nestedness to 3 levels
+let chests = [chest, makeNestedBox()]
+
+/// Check the results (all calculated correctly)
+countBoxesRecursively(box0)
+countBoxesRecursively(box1)
+countBoxesRecursively(chest)
+countBoxesRecursively(chests)
+
+// MARK: - Recursion: n!
+
+/// Iterative factorial
+/// - Parameter number: Some integer
+/// - Returns: Factorial result for some integer
 func factorialIterative(_ number: Int) -> Int {
-	var i = number - 1
+	/// The 1nd factor is the input number itself
 	var res = number
 	
+	/// The 2nd factor `(n-1)` is declared beforehand
+	var i = number - 1
 	
+	/// Until we reach `0` we will multilpy
 	while i != 0 {
+		/// The number by it's subtracted part
 		res *= i
+	
+		/// And decrement the counter
 		i -= 1
 	}
 	
+	/// In the end we just return the res
 	return res
 }
 
+/// Recursive factorial
+/// - Parameter number: Some integer
+/// - Returns: Factorial result for some integer
 func factorialRecursive(_ number: Int) -> Int {
+	/// Base case is when number us `0`
 	if number == 0 {
+		/// `0! = 1`
 		return 1
 	} else {
+		/// Return the number multiplied by `(number-1)!`
 		return number * factorialRecursive(number - 1)
 	}
 }
 
-print(countBoxesRecursively(chests))
-
-
+/// Both of the functions return the same value (`120`)
+let i = factorialIterative(5)
+let r = factorialRecursive(5)
---
+++ GROK-3-Recursion/Stack.playground/Contents.swift
@@ -0,0 +1,127 @@
+import Foundation
+
+// MARK: - Stack Imp
+
+/// Type implementing the main stack behaviour with integers as example
+/// - Note: last item is considered the `top` of the stack
+/// - Important: Swift `Colleciton` protocol not implemented to constrain the behaviour
+struct Stack<T: BinaryInteger> {
+	
+	// MARK: - Private
+	
+	/// Stored data
+	private var _storage = [T]()
+	
+	/// Stack count
+	private var _count = 0
+	
+	// MARK: - Initialization
+	
+	/// Instantiates the stack with items
+	/// - Parameter items: array of objects conforming to `BinaryInteger` protocol
+	public init(_ items: [T]) {
+		_storage = items
+	}
+	
+	/// Instantiates the stack with item
+	/// - Parameter items: object conforming to `BinaryInteger` protocol
+	public init(_ item: T) {
+		_storage.append(item)
+	}
+	
+	// MARK: - Methods
+	
+	/// Add element to the top of the stack
+	/// - Parameter item: Some item conforming to `BinaryInteger` protocol
+	/// - Complexity: `O(1)`
+	public mutating func push(_ item: T) {
+		_storage.append(item)
+		_count += 1
+	}
+	
+	
+	/// Removes the topmost element
+	/// - Returns: The former topmost element
+	/// - Complexity: O(1)
+	/// - Note: `@discardableResult` used since the return value is not always needed
+	@discardableResult
+	public mutating func pop() -> T {
+		_count -= 1
+		return _storage.remove(at: _storage.firstIndex(of: _storage.last!)!)
+	}
+	
+	/// Reverses the stack
+	/// - Important: This method has side-effects
+	/// - Complexity: `O(n)`, where `n` = last index number
+	public mutating func reversed() {
+		
+		var newStorage = [T]()
+		var i = _storage.count - 1
+		
+		while let item = _storage.last, i >= 0 {
+			newStorage.append(item)
+			print(newStorage)
+			pop()
+			i -= 1
+		}
+		
+		_storage = newStorage
+		
+		print("[REVERSED] \n \(description)")
+		
+	}
+	
+	// MARK: - Convenience
+	
+	/// Returns the topmost element of the collection
+	public func peek() -> T? {
+		_storage.last
+	}
+	
+	/// `true` if the stack lacks items
+	public var isEmpty: Bool {
+		_count == 0 && _storage.count == 0
+	}
+	
+	/// Stack count (public)
+	public var count: Int {
+		_count
+	}
+	
+}
+
+/// Stack textual representation
+extension Stack: CustomStringConvertible {
+	
+	public var description: String {
+		"""
+		--- top ---
+		\(_storage.map { "\t \($0)" }.reversed().joined(separator: "\n"))
+		--- bot ---
+		"""
+	}
+}
+
+/// Initialize from array literal
+extension Stack: ExpressibleByArrayLiteral {
+	
+	/// Int is taken for example
+	typealias ArrayLiteralElement = Int
+	
+	
+	/// Instantiates the stack from the array literal
+	/// - Parameter elements: Some integers
+	public init(arrayLiteral elements: Int...) {
+		guard let array = elements as? [T] else {
+			fatalError("Could not initialize from array literal")
+		}
+		_storage = array
+	}
+	
+}
+
+var stack = Stack([1, 2, 3, 4, 5, 6, 7])
+print(stack)
+stack.reversed()
+stack.reversed()
+stack.reversed()
