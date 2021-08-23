b5460fa95ed03d7cb4ffe839ab818d127a55d825
Dima <dmi.aksyonov@gmail.com>
2021-08-23 10:02:56 +0300

    GROK-5: WIP
    Signed-off-by: Dima <dmi.aksyonov@gmail.com>

--- GROK-5-HashTables/HashTables.playground/Contents.swift
+++ GROK-5-HashTables/HashTables.playground/Contents.swift
@@ -1,26 +1,33 @@
-// MARK: - Hashing Func Imp
+// MARK: - Hashing Funcs Imp
 
 /**
- - Note: all implementations are taken from here:
- 
+- Note: all implementations are taken from here:
 
- - This section introcudes some implementations of popular hashing algorithms and hash tables
- - Additional Sources:
-	- http://www.cse.yorku.ca/~oz/hash.html
-	- https://www.raywenderlich.com/206-swift-algorithm-club-hash-tables
 
- - Goals:
-	 - Understand what hashing is and why it is important
-	 - How it maps to hash tables
-	 - Play with some algorithms
-	 - Implement a hash table
+- This section introcudes some implementations of popular hashing algorithms and hash tables
+- Additional Sources:
+- http://www.cse.yorku.ca/~oz/hash.html
+- https://www.raywenderlich.com/206-swift-algorithm-club-hash-tables
+
+- Goals:
+- Understand what hashing is and why it is important
+- How it maps to hash tables
+- Play with some algorithms
+- Implement a hash table
 */
 
 /// Hashes the input string naively
 /// - Parameter input: String
 /// - Returns: Hasр string's corresponding unicode scalars
 func naiveHash(_ input: String) -> Int {
+	
+	/// Nothing special here: all input chars are mapped to their corresponding unicode numbers
 	let unicodeScalars = input.unicodeScalars.map { Int($0.value) }
+	
+	/// After the numbers were mapped, they're summed up and returned
+	/// For example:
+	///
+	///		naiveHash("AAA") -> [65, 65, 65].reduce(0, +) -> 195
 	return unicodeScalars.reduce(0, +)
 }
 
@@ -28,29 +35,42 @@ func naiveHash(_ input: String) -> Int {
 /// - Parameter input: String
 /// - Returns: Hasg string's corresponding unicode scalars
 func djb2Hash(_ input: String) -> Int {
+	
+	/// Input is again mapped to numbers
 	let scalars = input.unicodeScalars.map{ $0.value }
+	
+	/// The number `5831` is used in testing and resulted in fewer `collisions` and better `avalanching`
+	///
+	/// - Note: A collision occurs when more than one value to be hashed by a particular hash
+	/// function are mapped to the same slot in the table or data structure (hash table).
+	/// http://www.umsl.edu/~siegelj/information_theory/projects/HashingFunctionsInCryptography.html
+	///
+	/// - Note: A hash function demonstrates the `avalanche` effect if a single flipped bit
+	/// in the input causes a change of, on average, half the bits of the output.
+	/// https://www.wolfram.com/language/12/cryptography/demonstrate-the-avalanche-effect-of-a-hash-function.html
 	return scalars.reduce(5831) {
+		
+		/// Technically this is an operation of multiplying by `33`:
+		///
+		///		$0 * 33 + $0
+		///
+		/// But on most CPU;s it's bitShift equivalent is faster.
+		/// For example:
+		///
+		///		[1]		Int(5)   << 5 + Int(5)   = 5 * 33 - base equation
+		///		[2]		00000101 << 5 + 00000101 = 5 * 33 - binary conversion
+		///		[3]		10100000      + 00000101 = 5 * 33 - bits shifted (160 in res)
+		///		[4]		10100101                 = 5 * 33 - bits added (150 in res)
+		///		[5]		165                      = 165
+		///
+		/// Therefore the bitArray of a number, shifted by 5 to left and summed with itseld before shift
+		/// results in operation equal to multiplying this number by 33.
 		($0 << 5) &+ $0 &+ Int($1)
 	}
 }
 
-func sdbmHash(_ input: String) -> Int {
-	let scalars = input.unicodeScalars.map({ Int($0.value) })
-	return scalars.reduce(0) {
-		$1 &+ ($0 << 6) &+ ($0 << 16) - $0
-	}
+// MARK: - Utility
+
+func getIndex(from hash: Int, _ size: Int) -> Int {
+	return abs(hash) % size
 }
-
-
-func divideIntoParts(_ input: Double) -> (Double, Double) {
-  let randomUpperBound = Double.random(in: 0.0...0.61)
-  let random = Double.random(in: 0...Double(input)*randomUpperBound)
-
-  return (random, input - random)
-}
-
-print(divideIntoParts(5000.0))
-
-// (2354.392553621411, 2645.607446378589)
-// это не 4, а надо 4
-
--- GROK-5-HashTables/README.md
+++ GROK-5-HashTables/README.md
@@ -1,20 +1,46 @@
 # CH5 – Hash Tables
 
-### Applying Hash Tables
+### Additional References
 
-### Hash Tables Internals: Implementation
+- [Swift Algorithm Club: Hash Tables](https://www.raywenderlich.com/206-swift-algorithm-club-hash-tables)
 
-### Hash Tables Internals: Collisions
+### What is a Hash Table
+
+Hash tables are arrays. When a value is inserted into the hash table (usually made by key), the table uses this key to calculate element index . Adding a value of a different key puts it to the different index. Under the hood when the user introduces a new key, the hash table asks for key's hash value.
 
 ### Hash Tables Internals: Hash Functions
 
 Hash function in general maps the data of arbitrary size (or type) to a fixed-size values.  For instance it can be a function that accepts a string and returns an integer. 
 Sample declaration: `hashFunc(key: String) -> Int`.
 
+I play with hash functions [here](https://github.com/DimkaIsALifelongLearner/Grokking-Algorithms-Study-Notes/blob/GROK-5-hash-tables/GROK-5-HashTables/HashTables.playground/Contents.swift). 
+⚠️ - link may will need to be updated in future.
+
 Hash functions design should follow some rules:
 
-- mapping itself should get the same result each time. E.g. `hashFunc("apple") -> 4` should always return 4 with this input.
+- mapping itself should get the same result each time, e.g. `hashFunc("apple") -> 4` should always return 4 with this input.
 - mapping should not return same number for different inputs.
+
+It's fair to state that hash functions can produce enormous hashes, that can be negative. A common way to neat this up is to make modulo opertaion on absolute hash value, using the size of the array: `abs(hash) % array.count`.
+
+But here is one problem – collisions can be:
+
+```swift
+// MARK: - Simulating Collision
+
+getIndex(from: djb2Hash("cat" ), 5) -> 0
+getIndex(from: djb2Hash("cats"), 5) -> 0
+```
+
+### Hash Tables Internals: Collisions
+
+Chaining is the common way to overcome collisioning problems when designing a hash table. When the table is on this technique, each array element acts a role of a bucket with a list of zero to infinity key-value pairs.
+
+### Applying Hash Tables
+
+### Hash Tables Internals: Implementation
+
+Swift dictionary is itself a hash-table.
 
 ### Analysing Effectiveness
 
