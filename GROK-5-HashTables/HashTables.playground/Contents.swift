8431d29815414f81d8f632abc06799f77900ae57
Аксёнов Дмитрий Александрович <DAlAksenov@sberbank.ru>
2021-08-25 08:49:48 +0300

    GROK-5: WIP

--- GROK-5-HashTables/HashTables.playground/Contents.swift
+++ GROK-5-HashTables/HashTables.playground/Contents.swift
@@ -1,29 +1,26 @@
 // MARK: - Hashing Funcs Imp
 
 /**
-- Note: all implementations are taken from here:
-
-
 - This section introcudes some implementations of popular hashing algorithms and hash tables
 - Additional Sources:
 - http://www.cse.yorku.ca/~oz/hash.html
 - https://www.raywenderlich.com/206-swift-algorithm-club-hash-tables
 
 - Goals:
-- Understand what hashing is and why it is important
-- How it maps to hash tables
-- Play with some algorithms
-- Implement a hash table
+- understand what hashing is and why it is important;
+- how it maps to hash tables;
+- play with some algorithms;
+- implement a hash table.
 */
 
 /// Hashes the input string naively
 /// - Parameter input: String
 /// - Returns: Hasр string's corresponding unicode scalars
-func naiveHash(_ input: String) -> Int {
-	
+public func naiveHash(_ input: String) -> Int {
+
 	/// Nothing special here: all input chars are mapped to their corresponding unicode numbers
 	let unicodeScalars = input.unicodeScalars.map { Int($0.value) }
-	
+
 	/// After the numbers were mapped, they're summed up and returned
 	/// For example:
 	///
@@ -34,11 +31,11 @@ func naiveHash(_ input: String) -> Int {
 /// Hashes the input string using Bernstein's algorithm
 /// - Parameter input: String
 /// - Returns: Hasg string's corresponding unicode scalars
-func djb2Hash(_ input: String) -> Int {
-	
+public func djb2Hash(_ input: String) -> Int {
+
 	/// Input is again mapped to numbers
 	let scalars = input.unicodeScalars.map{ $0.value }
-	
+
 	/// The number `5831` is used in testing and resulted in fewer `collisions` and better `avalanching`
 	///
 	/// - Note: A collision occurs when more than one value to be hashed by a particular hash
@@ -49,7 +46,7 @@ func djb2Hash(_ input: String) -> Int {
 	/// in the input causes a change of, on average, half the bits of the output.
 	/// https://www.wolfram.com/language/12/cryptography/demonstrate-the-avalanche-effect-of-a-hash-function.html
 	return scalars.reduce(5831) {
-		
+
 		/// Technically this is an operation of multiplying by `33`:
 		///
 		///		$0 * 33 + $0
@@ -69,8 +66,112 @@ func djb2Hash(_ input: String) -> Int {
 	}
 }
 
-// MARK: - Utility
+// MARK: - Utilities
 
-func getIndex(from hash: Int, _ size: Int) -> Int {
+/// Obtains the index of the bucket in the hash-table
+/// - Parameters:
+///   - hash: Content hash
+///   - size: Array size (all buckets count)
+/// - Returns: Index of the element
+public func getIndex(from hash: Int, _ size: Int) -> Int {
 	return abs(hash) % size
+}
+
+// MARL: - Hash Table Imp
+
+/// Implements basic logic for hash table
+/// - Parameters:
+///   - Key: Hashable value that corresponds to the element's content
+///   - Value: Element's content
+public struct HashTable<Key: Hashable, Value> {
+
+	// MARK: - Private
+
+	/// Shorthand for element representation `[Key: Value]`
+	private typealias Element = (key: Key, value: Value)
+
+	/// Bucket is an array of elements as per theory
+	private typealias Bucket  = [Element]
+
+	/// This is the bucket array itself
+	private var buckets: [Bucket]
+
+	/// Returns total count of the table
+	/// - Note: cannot be set outside obviously
+	private(set) var count = 0
+
+	// MARK: - Public
+
+	/// Returns `true` if the table is empty
+	public var isEmpty: Bool {
+		return count == 0
+	}
+
+	/// Returns the first element of the collection
+	public var first: Element {
+
+	}
+
+	/// Returns the last element of the collection
+	public var last: Element {
+
+	}
+
+	/// Returns the textual representation of the hash table
+	public var description: String {
+
+	}
+
+	// MARK: - Initialization
+
+	/// Instantiates the copy of hashTable
+	/// - Parameter capacity: Integer length
+	public init(capacity: Int) {
+		assert(capacity > 0)
+
+		buckets = Array<Bucket>(
+			repeating: [],
+			count: capacity
+		)
+	}
+
+	// MARK: - CRUD: Create
+
+	// MARK: - CRUD: Read
+
+	func element(at index: Int) -> Element {  }
+
+	func value(at index: Int) -> Value {  }
+
+	func elements(at indexes: [Int])  -> [Element] {  }
+
+	func elements(at indexes: Int...) -> [Element] {  }
+
+	func values(at indexes: [Int])  -> [Value] {  }
+
+	func values(at indexes: [Int])  -> [Value] {  }
+
+	// MARK: - CRUD: Update
+
+	func set(value: Int, at index: Int) {  }
+
+	func set(values: [Int], at indexes: [Int]) {  }
+
+	func set(values: Int..., at indexes: Int...) {  }
+
+	// MARK: - CRUD: Delete
+
+	func removeElement(at index: Int) {  }
+
+	func removeElements(at indexes: [Int]) {  }
+
+	func removeElements(at indexes: Int...) {  }
+
+	func removeAll() {  }
+
+	func dropFirst() {  }
+
+	func dropFirst(_ items: Int) {  }
+
+	func dropLast(_ items: Int) {  }
 }
--- GROK-5-HashTables/README.md
+++ GROK-5-HashTables/README.md
@@ -36,6 +36,18 @@ getIndex(from: djb2Hash("cats"), 5) -> 0
 
 Chaining is the common way to overcome collisioning problems when designing a hash table. When the table is on this technique, each array element acts a role of a bucket with a list of zero to infinity key-value pairs.
 
+Consider the example below:
+
+```ceylon
+[1]: no items
+[2]: no items
+[3]: ––> ["firstName": "Steve"] ––> ["lastName": "Jobs"]
+```
+
+When `hashTable["lastName"]` is called, firstly hashing capabilities of the table engage and calculate the index of the content. But here's the catch – 3rd bucket has two linked items. So the second round will be to step through the list and match the corresponding key. After "lastName" was matched, "Jons" is returned.
+
+
+
 ### Applying Hash Tables
 
 ### Hash Tables Internals: Implementation
